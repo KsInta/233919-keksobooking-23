7f9167f5f433527ddeafcac99a8ce3546779599b
DESKTOP-S207EEP\Vitalik <Ks.Inta@mail.ru>
2021-06-19 16:29:12 +0300

    Отрисуй меня полностью (часть 1)

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,37 @@
-//Здесь будет происходить показ попапа при успешной отправке данных на сервер либо ошибке при отправке. Как зависимость сюда будет подключаться server.js
+import {createAds, getAccommodationType, GUESTS, ROOMS} from './data.js';
+import {getRussianCase, getRussianGenitiveCase} from './util.js';
+
+const map = document.querySelector('.map');
+const mapCanvas = map.querySelector('#map-canvas');
+const adCardTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const similarAds = createAds();
+
+const similarListFragment = document.createDocumentFragment();
+
+similarAds.forEach((ad) => {
+  const adElement = adCardTemplate.cloneNode(true);
+  adElement.querySelector('.popup__title').textContent = ad.offer.title;
+  adElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+  adElement.querySelector('.popup__text--price').textContent = `${ad.offer.price} ₽/ночь`;
+  adElement.querySelector('.popup__type').textContent = getAccommodationType(ad.offer.type);
+  adElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} ${getRussianCase(ad.offer.rooms, ROOMS)} для ${ad.offer.guests} ${getRussianGenitiveCase(ad.offer.guests, GUESTS)}`;
+  adElement.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+  const modifiers = ad.offer.features.map((feature) => `popup__feature--${feature}`);
+  adElement.querySelectorAll('.popup__feature').forEach((item) => {
+    const modifier = item.classList[1];
+    if (!modifiers.includes(modifier)) {
+      item.remove();
+    }
+  });
+  adElement.querySelector('.popup__description').textContent = ad.offer.description;
+  ad.offer.photos.forEach((item) => {
+    const img = adElement.querySelector('.popup__photo').cloneNode();
+    img.setAttribute('src', item);
+    adElement.querySelector('.popup__photos').appendChild(img);
+  });
+  adElement.querySelector('.popup__photo').remove();
+  similarListFragment.appendChild(adElement);
+});
+
+mapCanvas.appendChild(similarListFragment);
--- js/util.js
+++ js/util.js
@@ -27,6 +27,23 @@ const getRandomArray = (parentArray) => {
 
 const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-export {getRandomInteger, getRandomFloatNumber, getRandomArray, getRandomArrayElement};
+const getRussianCase = (integer, russianWord) => {
+  const remainder = integer % 10;
+  if (integer > 10 && integer < 20) {
+    return russianWord[2];
+  } else if (remainder > 1 && remainder < 5) {
+    return russianWord[1];
+  } else if (remainder === 1) {
+    return russianWord[0];
+  }
+  return russianWord[2];
+};
+
+const getRussianGenitiveCase = (integer, russianWord) => {
+  const remainder = integer % 10;
+  return remainder === 1 && integer !== 11 ? russianWord[0] : russianWord[1];
+};
+
+export {getRandomInteger, getRandomFloatNumber, getRandomArray, getRandomArrayElement, getRussianCase, getRussianGenitiveCase};
 
 //Этот модуль уберем когда настроим работу с сервером.
