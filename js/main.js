d6ac6762299f2c7a97500e6e2a032c9d0929a1da
DESKTOP-S207EEP\Vitalik <Ks.Inta@mail.ru>
2021-06-20 17:01:43 +0300

    Выполнил задание 'Правда или действие (часть 1)'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import './popup.js';
+import './user-form.js';
 
 //Точка входа. Здесь в будущем подключим user-form.js, map.js, popup.js и наверно filter.js. Data.js под вопросом (filter.js тоже не уверен пока). Server.js как зависимость будет подключаться в другие модули.
